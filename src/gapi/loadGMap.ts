b9cd2a37a5f8e05c67a2fc41646b355cbb0824ff
Hillal Kumar Roy <hillolroy67@gmail.com>
2021-09-11 11:59:02 +0530

    google not define fixed

--- src/gapi/loadGMap.ts
+++ src/gapi/loadGMap.ts
@@ -7,10 +7,15 @@ export const loadGMaps = (): Promise<typeof google> => {
       script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD44_D7amWWfM77MCt9lZXCb_iP5_TvcvU&callback=initialize&libraries=&v=weekly";
       script.id = "googleMaps";
       document.body.appendChild(script);
-      (window as any).initialize = () => { resolve(google) };
+      (window as any).initialize = () => { 
+        if(window.google)
+        resolve(window.google)
+        else rejects() 
+      };
       script.onerror = rejects
     }
-    if (existingScript) resolve(google);
+    else if (existingScript && window.google) resolve(window.google);
+    else rejects()
   })
 
 };
